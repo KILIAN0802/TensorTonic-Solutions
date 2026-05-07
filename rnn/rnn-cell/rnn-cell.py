9da8c20ffc76b3daf64cfea389e82021c2dfe858
Nguyen Tuan Dat <nguyendat08022004@gmail.com>
2026-05-07 10:10:21 +0700

    Update solution for rnn-cell

--- rnn/rnn-cell/rnn-cell.py
+++ rnn/rnn-cell/rnn-cell.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def rnn_cell(x_t: np.ndarray, h_prev: np.ndarray, 
              W_xh: np.ndarray, W_hh: np.ndarray, b_h: np.ndarray) -> np.ndarray:
-
+# Viết theo cột
     h_t = np.tanh(W_xh@x_t+W_hh@h_prev+b_h)
     
     return h_t
